7c028eeaccb19c0284f30d7b30d3ccd58e8c99f5
Christopher Ebron <cebron88@gmail.com>
2023-01-01 20:47:36 -0500

    inital commit

---
+++ Bagels.js
@@ -0,0 +1,9 @@
+console.log('I am thinking of a 3-digit number. Try to guess what it is\n'+
+'Here are some clues:\n'+
+'When I say:    That means:\n'+
+'  Pico         One digit is correct but in the wrong position.\n'+
+'  Fermi        One digit is correct and in the right position.\n'+
+'  Bagels       No digit is correct.\n'+
+'I have thought up a number.\n'+
+' You have 10 guesses to get it.')
+
