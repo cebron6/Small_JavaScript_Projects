dc6e0285f3efc4a72e79c3c7e4f7ad843014bb7a
Christopher Ebron <cebron88@gmail.com>
2023-02-15 13:53:07 -0500

    Board is complete

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,20 +2,20 @@
 console.log("Welcome to Tic Tac Toe")
 
 const Board = [
-  [" "," "," "],
-  [" "," "," "],
-  [" "," "," "]
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
 ];
 
-Board[0][2] = "x";
+
 
 console.log(
 `-------------
-| ${Board[0][0]} | ${Board[0][0]} | ${Board[0][0]} |
+| ${Board[0][0]} | ${Board[0][1]} | ${Board[0][2]} |
 -------------
-| ${Board[0][0]} | ${Board[0][0]} | ${Board[0][0]} |
+| ${Board[1][0]} | ${Board[1][1]} | ${Board[1][2]} |
 -------------
-| ${Board[0][0]} | ${Board[0][0]} | ${Board[0][0]} |
+| ${Board[2][0]} | ${Board[2][1]} | ${Board[2][2]} |
 -------------
 `);
 //Create players
